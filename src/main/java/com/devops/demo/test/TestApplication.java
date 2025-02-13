cbd922d06f416757af0f17dcc176ee469be7fa4f
Durgaprasad <prasadmca718@gmail.com>
2025-02-13 07:34:59 +0530

    comments added

---
+++ src/main/java/com/devops/demo/test/TestApplication.java
@@ -0,0 +1,14 @@
+package com.devops.demo.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestApplication {
+
+	public static void main(String[] args) {
+		// This is main class
+		SpringApplication.run(TestApplication.class, args);
+	}
+
+}
